6c3f8f83146b5245868ce56656a3ed5607e5b4f6
Tolga <130807536+tatmaca77@users.noreply.github.com>
2023-09-13 11:07:00 +0200

    API Manuel-Automation Testing

---
+++ src/test/java/get_request/Get01.java
@@ -0,0 +1,25 @@
+package get_request;
+
+import org.junit.Test;
+
+public class Get01 {
+
+
+    /*
+    Given
+      https://restful-booker.herokuapp.com/booking/55
+    When
+      Kullanici URL'e bir GET request gönderir
+    Then
+      HTTP Status Code 200 olmali
+    And
+      Content Type "application/json" olmali
+    And
+      Status Line "HTTP/1.1 200 OK" olmali
+     */
+
+    @Test   // Main'den kurtardi Run butonu acildi !!!!
+    public void get01 (){
+
+    }
+}
